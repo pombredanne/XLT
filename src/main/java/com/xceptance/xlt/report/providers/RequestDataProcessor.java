dac7541b7b9e60c1d313114872c20f570fbe08ca
Rene Schwietzke <r.schwietzke@xceptance.net>
2020-07-19 18:22:47 +0200

    Larger defaults to avoid growing too much

--- src/main/java/com/xceptance/xlt/report/providers/RequestDataProcessor.java
+++ src/main/java/com/xceptance/xlt/report/providers/RequestDataProcessor.java
@@ -62,12 +62,12 @@ public class RequestDataProcessor extends BasicTimerDataProcessor
      * A set of hash codes generated from URL strings. Used to determine the number of distinct URLs used. Since storing
      * the URL strings can be memory-consuming, only their hash code is stored.
      */
-    private final TIntSet distinctUrlHashCodeSet = new TIntHashSet(127);
+    private final TIntSet distinctUrlHashCodeSet = new TIntHashSet(1271);
 
     /**
      * A set of distinct URLs. Contains at most {@link #MAXIMUM_NUMBER_OF_URLS} entries.
      */
-    private final Set<String> distinctUrlSet = new HashSet<String>(MAXIMUM_NUMBER_OF_URLS);
+    private final Set<String> distinctUrlSet = new HashSet<String>(2 * MAXIMUM_NUMBER_OF_URLS + 1);
 
     /**
      * The configured runtime segment boundaries. May be an empty array.
